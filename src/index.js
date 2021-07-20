a5bbfa98710ef183d1d260fc8cd46ba5dd836b5f
Tsyganov Andrey <andrytsy@mail.ru>
2021-07-20 21:55:06 +0300

    auth modal

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const {
 
 const email = 'mail.projects@yandex.ru';
 const pass = '1QazxsW@';
+// https://www.tiktok.com/login/phone-or-email/email
 
 (async () => {
     try {
@@ -18,33 +19,56 @@ const pass = '1QazxsW@';
             executablePath: '/Applications/Firefox.app/Contents/MacOS/firefox-bin', // mac
         });
         const page = await browser.newPage();
+
         await page.goto('https://www.tiktok.com');
-        await page.waitForSelector('.cookie-banner')
-        await page.waitForSelector('.button-wrapper button')
-        await delay(1204)
-        await page.click('.button-wrapper button')
-        await page.waitForSelector('.login-button')
-        await delay(1204)
-        await page.click('.login-button')
-        await page.waitForSelector('.login-frame-container')
-        await page.waitForSelector('iframe[src^="https://www.tiktok.com/login/"]')
+        await page.waitForSelector('.cookie-banner');
+        await page.waitForSelector('.button-wrapper button');
+        await page.click('.button-wrapper button');
+        await page.waitForSelector('.login-button');
+        await page.click('.login-button');
+        await page.waitForSelector('.login-frame-container');
+        await page.waitForSelector('iframe[src^="https://www.tiktok.com/login/"]');
         await page.evaluate(async () => {
-            const authFrame = document.querySelector('iframe[src^="https://www.tiktok.com/login/"]').contentWindow
+            const authFrame = document.querySelector('iframe[src^="https://www.tiktok.com/login/"]').contentWindow;
+
             authFrame.onload = () => {
-                const authFrameMutationCallback = (mutationsList, observer) => {
+                const observer = new MutationObserver((mutationsList, observer) => {
                     mutationsList.forEach((mutation) => {
                         if (mutation.addedNodes.length) {
-                            console.log('mutation.addedNodes[0]', mutation.addedNodes[0])
+                            const isTiktokApp = mutation.addedNodes[0].className.includes('tiktok-app-container-');
+                            const isEmailAuth = mutation.addedNodes[0].className.includes('tiktok-web-body-');
+                            const isFormContainer = mutation.addedNodes[0].className.includes('form-container-');
 
-                            if (mutation.addedNodes[0].classList?.contains('tiktok-app-container-')) {
+                            // console.log('mutation.addedNodes', mutation.addedNodes);
 
-                                // mutation.addedNodes[0].click();
-                                // observer.disconnect();
+                            if (isTiktokApp) {
+                                const authMethods = authFrame.document.querySelectorAll('div[class^="channel-item-wrapper-"]');
+
+                                authMethods[1].click();
+                            } else if (isEmailAuth) {
+                                const isEmailAuthButton = authFrame.document.querySelectorAll('div[class^="title-wrapper-"] a');
+
+                                isEmailAuthButton[0].click();
+                            } else if (isFormContainer) {
+                                const isPasswordInput = mutation.addedNodes[0].firstChild.className.includes('move-warning-');
+                                const inputWrapper = mutation.addedNodes[0].firstChild;
+
+                                setTimeout(() => {
+                                    if (isPasswordInput) {
+                                        inputWrapper.lastChild.value = '1QazxsW@';
+                                    } else {
+                                        inputWrapper.lastChild.value = 'mail.projects@yandex.ru';
+                                    }
+
+                                    inputWrapper.lastChild.dispatchEvent(new Event('input'));
+                                    console.log('inputWrapper.lastChild', inputWrapper.lastChild);
+                                }, 1000);
                             }
+
+                            // observer.disconnect();
                         }
-                    })
-                }
-                const observer = new MutationObserver(authFrameMutationCallback);
+                    });
+                });
 
                 observer.observe(authFrame.document.body, {
                     characterData: true,
@@ -52,12 +76,12 @@ const pass = '1QazxsW@';
                     childList: true,
                     subtree: true,
                 });
-            }
-        })
+            };
+        });
 
         // await browser.close();
     } catch (e) {
-        console.error(e)
+        console.error(e);
         await browser.close();
     }
 })();
