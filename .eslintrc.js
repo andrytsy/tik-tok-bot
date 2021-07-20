39186d24550ab4a1ba226d6617e204fba3da6883
Tsyganov Andrey <andrytsy@mail.ru>
2021-07-20 18:08:42 +0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,67 @@
-/* eslint no-use-before-define: 0 */
 module.exports = {
   root: true,
   env: {
-    es6: true
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-  extends: [  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
+  extends: 'airbnb-base',
   rules: {
-    indent: ['error', 4, { SwitchCase: 1 }],
-    'no-mixed-spaces-and-tabs': 0,
-    'no-tabs': 0,
-    "comma-dangle": ["error", {
-      "arrays": "always",
-      "objects": "always",
-      "imports": "never",
-      "exports": "never",
-      "functions": "never"
-    }]
+    'brace-style': ['error', '1tbs', {
+      allowSingleLine: false,
+    }],
+    'semi': [2, 'always'],
+    camelcase: 'warn',
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    curly: ['error', 'all'],
+    'default-case': 'off',
+    indent: ['error', 4, {
+      SwitchCase: 1,
+    }],
+    'max-len': 'off',
+    'newline-after-var': ['error', 'always'],
+    'newline-before-return': 'error',
+    'no-console': 'error',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+      'Literal[value="null"]',
+      'BinaryExpression[operator="in"]',
+    ],
+    'no-script-url': 'warn',
+    'no-shadow': 'off',
+    'no-shadow-restricted-names': 'warn',
+    'no-useless-concat': 'off',
+    'no-unused-vars': 'off',
+
+    'object-curly-spacing': ['error', 'never'],
+    'object-shorthand': ['error', 'always', {
+      avoidQuotes: false,
+    }],
+    'prefer-template': 'off',
+
+    'node/no-callback-literal': 'off',
+
+    'import/dynamic-import-chunkname': ['error', {
+      importFunctions: ['dynamicImport'],
+      webpackChunknameFormat: '[0-9a-zA-Z-_/.]+',
+    }],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      json: 'never',
+      jsx: 'never',
+      mjs: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
+    'import/no-dynamic-require': 'off',
+    'import/prefer-default-export': 'off',
   },
-  overrides: [
-    {
-      files: ['.js'],
-      rules: {
-        indent: 'off'
-      }
-    }
-  ]
-}
+  overrides: [],
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer-core');
+const {
+    delay,
+} = require('./helpers');
 
 const email = 'mail.projects@yandex.ru';
 const pass = '1QazxsW@';
@@ -11,43 +14,53 @@ const pass = '1QazxsW@';
             args: [
                 '-wait-for-browser',
             ],
-            executablePath: 'C:/Program Files/Mozilla Firefox/firefox.exe', // windows
+            // executablePath: 'C:/Program Files/Mozilla Firefox/firefox.exe', // windows
+            executablePath: '/Applications/Firefox.app/Contents/MacOS/firefox-bin', // mac
         });
         const page = await browser.newPage();
         await page.goto('https://www.tiktok.com');
+        await page.waitForSelector('.cookie-banner')
+        await page.waitForSelector('.button-wrapper button')
+        await delay(1204)
+        await page.click('.button-wrapper button')
         await page.waitForSelector('.login-button')
+        await delay(1204)
         await page.click('.login-button')
         await page.waitForSelector('.login-frame-container')
         await page.waitForSelector('iframe[src^="https://www.tiktok.com/login/"]')
-        await page.evaluate(() => {
-            const authFrame = document.querySelector('iframe[src^="https://www.tiktok.com/login/"]').contentWindow
-            const authMethods = authFrame.document.querySelectorAll('div[class^="channel-item-wrapper-"]')
+        await page.evaluate(async () => {
+            const authFrame = document.querySelector('iframe[src^="https://www.tiktok.com/login/"]').onload(() => {
+                const authFrame = document.querySelector('iframe[src^="https://www.tiktok.com/login/"]').contentWindow
+                const authMethods = authFrame.document.querySelectorAll('div[class^="channel-item-wrapper-"]')
 
-            console.log('authMethods', authMethods)
+                console.log('authMethods', authMethods)
 
-            const authFrameMutationCallback = (mutationsList, observer) => {
-                mutationsList.forEach((mutation) => {
-                    console.log('mutation', mutation)
+                if (authMethods.length) {
+                    authMethods[1].click();
+                } else  {
+                    const authFrameMutationCallback = (mutationsList, observer) => {
+                        mutationsList.forEach((mutation) => {
+                            if (mutation.addedNodes.length) {
+                                console.log('mutation.addedNodes[0]', mutation.addedNodes[0])
 
-                    if (mutation.addedNodes.length) {
-                        if (mutation.addedNodes[0].classList?.contains('channel-item-wrapper-')) {
-                            mutation.addedNodes[0].click()
-                        }
-
-                        if (false) {
-                            observer.disconnect();
-                        }
+                                if (mutation.addedNodes[0].classList?.contains('channel-item-wrapper-')) {
+                                    mutation.addedNodes[0].click();
+                                    observer.disconnect();
+                                }
+                            }
+                        })
                     }
-                })
-            }
-            const observer = new MutationObserver(authFrameMutationCallback);
+                    const observer = new MutationObserver(authFrameMutationCallback);
 
-            observer.observe(authFrame.document, {
-                attributes: true,
-                childList: true,
-                subtree: true,
-            });
-        });
+                    observer.observe(authFrame.document.body, {
+                        characterData: true,
+                        attributes: true,
+                        childList: true,
+                        subtree: true,
+                    });
+                }
+            })
+        })
 
         // await browser.close();
     } catch (e) {
